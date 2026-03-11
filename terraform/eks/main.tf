70de161d955849e08429959843e5458b52b728ab
Ananya Priyadarshini <ananyapriyadarshini03@gmail.com>
2026-03-11 20:01:12 +0530

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,1 +1,21 @@
 
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
+
+  cluster_name    = "tetris-cluster"
+  cluster_version = "1.30"
+
+  vpc_id     = "your-vpc-id"
+  subnet_ids = ["subnet-1", "subnet-2"]
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 3
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
